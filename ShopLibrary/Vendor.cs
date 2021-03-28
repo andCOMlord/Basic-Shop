d0880287f0fcf51bba3252b106de27cd1194f76f
Andreas Progkri <79377278+andCOMlord@users.noreply.github.com>
2021-03-28 16:53:24 +0100

    initial commit

---
+++ ShopLibrary/Vendor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopLibrary
+{
+    public class Vendor
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Commission { get; set; }
+        public decimal PaymentDue { get; set; }
+        public string venDiplay
+        {
+            get
+            {
+                return string.Format("{0} {1} - £{2}", FirstName, LastName, PaymentDue);
+            }
+        }
+
+        public Vendor()
+        {
+            Commission = .5;
+        }
+    }
+}
